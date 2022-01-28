85cdee8ce502bb90240244fe4eacfc153097e18e
bradwbaker <bradleybaker@email.arizona.edu>
2022-01-27 19:25:38 -0800

    adds logisitic regression and updates routes for final working app

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,67 +1,88 @@
-from flask import Flask, render_template
-from .models import DB, User, Tweet
+"""This is what brings the application together"""
 from os import getenv
+from flask import Flask, render_template, request
+from .predict import predict_user
+from .models import DB, User, Tweet
 from .twitter import add_or_update_user
 
+
 def create_app():
-
+    """
+    The main app function for twitoff.
+    Brings everything together.
+    """
+    # __name__ is the name of the current path module
     app = Flask(__name__)
-    
-    # configuration variable to our app
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # connect our database to the app object
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     DB.init_app(app)
 
-    @app.route("/")
-    def home_page():
-        users = User.query.all()
-        print(users)
-        return render_template('base.html', title='Home', users=users)
-    
-    @app.route('/populate')
-    # test my database funtionality
-    #by inserting some fake data into the DB
-    def populate():
-        
-        add_or_update_user('nasa')
-        add_or_update_user('euphoriaHBO')
-        add_or_update_user('RyanAllred')
-
-        return render_template('base.html', title='Populate')
+    @app.route('/')
+    def root():
+        return render_template('base.html', title="Home", users=User.query.all())
 
     @app.route('/update')
-    # test my database funtionality
-    #by inserting some fake data into the DB
     def update():
-        
+        '''update all users'''
         usernames = get_usernames()
         for username in usernames:
             add_or_update_user(username)
-
-        return render_template('base.html', title='Update User Tweets')
+        return render_template('base.html', title='All users have been updated.')
 
     @app.route('/reset')
     def reset():
-        # do some database stuff
-        # drop old DB tables
-        # remake new db tables
-        #remove everything from the DB
         DB.drop_all()
-        # recreate the User and Tweet tables
-        # so that they're ready to be used (inserted into)
         DB.create_all()
-        return render_template('base.html', title='Reset Database')
+        return render_template("base.html", title="Database has been reset.")
+
+    @app.route('/user', methods=["POST"])
+    @app.route('/user/<username>', methods=["GET"])
+    def user(username=None, message=''):
+        # we either take name that was passed in or we pull it
+        # from our request.values which would be accessed through the
+        # user submission
+        username = username or request.values['user_name']
+        try:
+            if request.method=='POST':
+                add_or_update_user(username)
+                message = f"User {username} has been succesfully added!"
+            tweets = User.query.filter(User.username == username).one().tweets
+        except Exception as e:
+            message = f"Error adding {username}: {e}"
+            tweets = []
+
+        return render_template("user.html", title=username, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=["POST"])
+    def compare():
+        user0, user1 = sorted(
+            [request.values['user0'], request.values["user1"]])
+
+        if user0 == user1:
+            message = "Cannot compare users to themselves!"
+
+        else:
+            # prediction returns a 0 or 1
+            prediction = predict_user(
+                user0, user1, request.values["tweet_text"])
+            message = "'{}' is more likely to be said by {} than {}!".format(
+                request.values["tweet_text"],
+                user1 if prediction else user0,
+                user0 if prediction else user1
+            )
+
+        return render_template('prediction.html', title="Prediction", message=message)
 
     return app
 
 
 def get_usernames():
-    # get all of the usernames of existing users
+    # get all usernames of existing users
     Users = User.query.all()
-    usernames = []
+    usernames= []
     for user in Users:
         usernames.append(user.username)
-        
+
     return usernames
---
+++ twitoff/predict.py
@@ -0,0 +1,45 @@
+from .models import User
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+from .twitter import vectorize_tweet
+
+def predict_user(user0_username, user1_username, hypo_tweet_text):
+    '''
+    Determine and returns which user is more likely to say a given tweet
+    Example run: predict_user("elonmusk", "jackblack", "Tesla cars go vroom")
+    Returns a 0 (user0_name: "elonmusk") or a 1 (user1_name: "jackblack")
+    '''
+
+    # Query for the two users so that we can get their word embeddings
+    user0 = User.query.filter(User.username == user0_username).one()
+    user1 = User.query.filter(User.username == user1_username).one()
+
+    # get the word embeddings of the tweets of both users 0 and 1. 
+    user0_vects = np.array([tweet.vect for tweet in user0.tweets])
+    user1_vects = np.array([tweet.vect for tweet in user1.tweets])
+
+    # combine the two users word embeddings into on e big *2D* NumPy array
+    # this is essentially our X matrix for training our logistic regression
+    vects = np.vstack([user0_vects, user1_vects])
+
+
+    # create a np array to represent the y vector 
+    # was the author of any given word embedding
+
+    labels = np.concatenate([np.zeros(len(user0.tweets)),
+                             np.ones(len(user1.tweets))])
+
+
+    # import and train our logisitic regression
+    log_reg = LogisticRegression()
+
+    # train our logistic regression
+    log_reg.fit(vects, labels)
+
+    # get the word embeddigs for our hypo_tweet_text
+    hypo_tweet_vect = vectorize_tweet(hypo_tweet_text)
+
+    # generate a prediction
+    prediction = log_reg.predict([hypo_tweet_vect])
+
+    return prediction[0]
